ce93e88719a40f5377f080dd186f49e9586a6dd1
Brittany Hancock <brittany.hancock@student.centralia.edu>
2023-11-29 03:31:16 -0800

    Add project files.

---
+++ ER_PriorityQueue_HancockB_Project/Patient.cs
@@ -0,0 +1,35 @@
+﻿
+namespace ER_Priority_Queue_Hancock_Project
+{
+    internal class Patient
+    {
+        private string _firstName;
+        private string _lastName;
+        private DateOnly _dateOfBirth;
+        private int _priority;
+
+        public Patient(string FirstName, string LastName, DateTime DateOfBirth, int Priority)
+        {
+            _firstName = FirstName;
+            _lastName = LastName;
+            decimal age = (DateTime.Now - DateOfBirth).Days / 365;
+            if (age < 21 || age >= 65)
+            {
+                Priority = 1;
+            }
+            DateOnly dateOfBirth = DateOnly.FromDateTime(DateOfBirth);
+            _dateOfBirth = dateOfBirth;
+            _priority = Priority;
+        }
+
+        public int Priority
+        {
+            get { return _priority; }
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0,-15} {1,-20} {2,10:MM/dd/yyyy} {3,8}", _firstName, _lastName, _dateOfBirth, _priority);
+        }
+    }
+}
---
+++ ER_PriorityQueue_HancockB_Project/Program.cs
@@ -0,0 +1,168 @@
+﻿// Brittany Hancock
+// IT113
+// Notes:  None
+// Behaviors not implemented and why: n/a
+
+using System.IO;
+
+namespace ER_Priority_Queue_Hancock_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "Patients.csv";
+            PriorityQueue<Patient, int> priorityQueue = new PriorityQueue<Patient, int>();
+            priorityQueue = ReadPatientListFromCsv(path);
+
+
+            Console.Write(Menu());
+            bool go = true;
+            while (go)
+            {
+                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+
+                char keyChar = char.ToUpper(keyInfo.KeyChar);
+
+                if (keyChar == 'A')
+                {
+                    Console.WriteLine("Add action triggered");
+                    int patientCount = AddPatient(priorityQueue);
+                    Console.WriteLine(patientCount + " Patients in Queue");
+                    Console.WriteLine();
+                    Console.Write(Menu());
+                }
+                else if (keyChar == 'P')
+                {
+                    Console.Clear();
+                    Console.WriteLine("Process action triggered");
+                    ProcessPatient(priorityQueue);
+                    Console.WriteLine();
+                    Console.Write(Menu());
+                }
+                else if (keyChar == 'L')
+                {
+                    Console.WriteLine("List action triggered");
+                    Census(priorityQueue);
+                    Console.WriteLine();
+                    Console.Write(Menu());
+                }
+                else if (keyChar == 'Q')
+                {
+                    Console.WriteLine("Exit action triggered.  Exiting program.");
+                    go = false;
+                }
+                else
+                {
+                    Console.WriteLine("\nInvalid key.  Please use 'A', 'P', 'L', or 'E'." +
+                        "\nPlease select an option:  ");
+                }
+            }
+        }
+
+        static void ProcessPatient(PriorityQueue<Patient, int> priorityQueue)
+        {
+            if (priorityQueue.Count > 0)
+            {
+                Patient patient = priorityQueue.Dequeue();
+                Console.WriteLine("\n" + String.Format("{0,-15} {1,-20} {2,-17} {3,8}", "FIRST NAME", "LAST NAME", "DOB", "PRIORITY"));
+                Console.WriteLine(patient.ToString());
+            }
+            else
+            {
+                Console.WriteLine("The queue is empty.  No patients to process.");
+            }
+        }
+
+        static PriorityQueue<Patient, int> Census(PriorityQueue<Patient, int> priorityQueue)
+        {
+            int count = priorityQueue.Count;
+
+            List<Patient> tempPatientList = new();
+            Console.WriteLine("\n" + String.Format("{0,-15} {1,-20} {2,-17} {3,8}", "FIRST NAME", "LAST NAME", "DOB", "PRIORITY"));
+            for (int i = 0; i < count; i++)
+            {
+                if (priorityQueue.TryDequeue(out Patient copyPatient, out int priority))
+                {
+                    Console.WriteLine(copyPatient.ToString());
+                    tempPatientList.Add(copyPatient);
+                }
+            }
+            foreach (Patient patient in tempPatientList)
+            {
+                priorityQueue.Enqueue(patient, patient.Priority);
+            }
+
+            return priorityQueue;
+        }
+
+        static PriorityQueue<Patient, int> ReadPatientListFromCsv(string path)
+        {
+            PriorityQueue<Patient, int> priorityQueue = new PriorityQueue<Patient, int>();
+
+            string[] lines = File.ReadAllLines(path);
+
+            foreach (string line in lines)
+            {
+
+                string[] values = line.Split(',');
+
+                string firstName = values[0];
+                string lastName = values[1];
+                bool tryDate = DateTime.TryParse(values[2], out DateTime dateOfBirth);
+                bool tryPriority = int.TryParse(values[3], out int priority);
+                if (tryDate && tryPriority)
+                {
+                    Patient patient = new Patient(firstName, lastName, dateOfBirth, priority);
+                    priorityQueue.Enqueue(patient, patient.Priority);
+                }
+            }
+            return priorityQueue;
+        }
+
+        static string Menu()
+        {
+            return "Menu Options:\n   - Press 'A' to Add a New Patient" +
+                "\n   - Press 'P' to Process the Current Patient" +
+                "\n   - Press 'L' to List All Patients in Queue" +
+                "\n   - Press 'Q' to Quit the Application" +
+                "\n\nPlease select an option:  ";
+        }
+
+        static int AddPatient(PriorityQueue<Patient, int> priorityQueue)
+        {
+            Console.WriteLine("Please enter the following details:");
+            Console.Write("Enter First Name:  ");
+            string firstName = Console.ReadLine();
+            Console.Write("Enter Last Name:  ");
+            string lastName = Console.ReadLine();
+
+            DateTime dateOfBirth;
+            while (true)
+            {
+                Console.Write("Enter Date of Birth (mm/dd/yyyy):  ");
+                string dobInput = Console.ReadLine();
+                if (DateTime.TryParse(dobInput, out dateOfBirth))
+                {
+                    break;
+                }
+                Console.WriteLine("Invalid date format.  Please try again.");
+            }
+
+            int priority;
+            while (true)
+            {
+                Console.Write("Enter Priority (1-5, 1 being lowest and 5 highest):  ");
+                if (int.TryParse(Console.ReadLine(), out priority))
+                {
+                    break;
+                }
+                Console.WriteLine("Invalid priority.  Please enter a number between 1 and 5.");
+            }
+            Patient patient = new Patient(firstName, lastName, dateOfBirth, priority);
+            priorityQueue.Enqueue(patient, patient.Priority);
+
+            return priorityQueue.Count;
+        }
+    }
+}
